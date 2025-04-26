24d6c63b3cd800479357f54f2da89709ba256a88
mint <nahithack@gmail.com>
2025-04-26 22:39:57 +0200

    remove commented-out code

--- internal/monitoring/cpu.go
+++ internal/monitoring/cpu.go
@@ -57,32 +57,6 @@ func (s *SystemCPUInfo) GetInfo() (StaticCPUInfo, error) {
 	})
 
 	return s.info, s.initErr
-
-	/*
-		info, err := cpu.Info()
-		if err != nil {
-			return StaticCPUInfo{}, fmt.Errorf("failed to get CPU info: %w", err)
-		}
-		if len(info) == 0 {
-			return StaticCPUInfo{}, fmt.Errorf("no CPU info available")
-		}
-
-		logical, err := cpu.Counts(true)
-		if err != nil {
-			return StaticCPUInfo{}, fmt.Errorf("failed to get logical cores: %w", err)
-		}
-
-		physical, err := cpu.Counts(false)
-		if err != nil {
-			return StaticCPUInfo{}, fmt.Errorf("failed to get physical cores: %w", err)
-		}
-
-		return StaticCPUInfo{
-			ModelName:     info[0].ModelName,
-			LogicalCores:  logical,
-			PhysicalCores: physical,
-		}, nil
-	*/
 }
 
 // ==================== CPU Metrics ====================
