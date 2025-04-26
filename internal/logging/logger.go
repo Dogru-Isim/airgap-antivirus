71ea093d4b55cebd4dfa32562ee40043fddef773
mint <nahithack@gmail.com>
2025-04-26 22:55:44 +0200

    create initial CpuLogger struct

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -1,9 +1,20 @@
 package logging
 
 import (
+	"errors"
 	"io"
 )
 
+//============================ Logger Interface ============================//
+
 type Logger interface {
 	Log(writer io.Writer, log string) error
+}
+
+//============================ CpuLogger Struct ============================//
+
+type CpuLogger struct{}
+
+func (cpuLogger *CpuLogger) Log(writer io.Writer, logMessage string) error {
+	return errors.New("Non-Implemented")
 }
